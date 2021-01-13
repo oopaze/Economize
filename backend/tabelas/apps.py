880ec792df007eeecd777474fd46923e08777234
Jose Pedro <jgomes@geobit.com>
2021-01-13 13:50:10 -0300

    changing name of folder

--- backend/tabelas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TabelasConfig(AppConfig):
-    name = 'tabelas'
--- backend/usuario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserListView, UserPasswordChangeView, UserCreateView, UserDeleteView
-
-urlpatterns = [
-    path('', UserListView.as_view(), name="user-list"),
-    path('add/', UserCreateView.as_view(), name="user-add"),
-    path('delete/<int:pk>/', UserDeleteView.as_view(), name="user-delete"),
-    path('password/change/', UserPasswordChangeView.as_view(), name="user-password-change")
-]
